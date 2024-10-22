392bb66407e636b110a887b6e6991b1ff5c900f3
Ivan Dekhtiarov <ivan.dekhtiarov@gmail.com>
2024-10-22 11:14:52 -0700

    Initial Commit

---
+++ CountriesList/CountriesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesListApp.swift
+//  CountriesList
+//
+//  Created by divan on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CountriesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
